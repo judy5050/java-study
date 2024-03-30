cdaa6ce09a4073c69784d6126ea760bcfaf50366
judy5050 <qkrgywjd456@naver.com>
2024-03-30 15:05:49 +0900

    feat: 다형성 2~3

---
+++ java-basic/src/poly/diamond/DiamondMain.java
@@ -0,0 +1,14 @@
+package poly.diamond;
+
+public class DiamondMain {
+
+    public static void main(String[] args) {
+        InterfaceA interfaceA = new Child();
+        interfaceA.methodA();
+        interfaceA.methodCommon();
+
+        InterfaceB interfaceB = new Child();
+        interfaceB.methodB();
+        interfaceB.methodCommon();
+    }
+}
