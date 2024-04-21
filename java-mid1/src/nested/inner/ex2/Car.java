f9b6aee0baddd5f0f71a94f1d92fa9ad27dad4a8
judy5050 <qkrgywjd456@naver.com>
2024-04-21 12:53:33 +0900

    feat: 실전자바 중급 1편 - 중첩 클래스, 내부 클래스 1

---
+++ java-mid1/src/nested/inner/ex2/Car.java
@@ -0,0 +1,38 @@
+package nested.inner.ex2;
+
+
+public class Car {
+    private String model;
+    private int chargeLevel;
+    private Engine engine;
+
+    public Car(String model, int chargeLevel) {
+        this.model = model;
+        this.chargeLevel = chargeLevel;
+        this.engine = new Engine();
+    }
+
+    //Engine 에서만 사용하는 메서드
+    public String getModel() {
+        return model;
+    }
+
+    //Engine 에서만 사용하는 메서드
+    public int getChargeLevel() {
+        return chargeLevel;
+    }
+
+    public void start() {
+        engine.start();
+        System.out.println(model + " 시작완료");
+    }
+
+    private class Engine {
+
+
+        public void start() {
+            System.out.println("충전 레벨 확인: " + chargeLevel);
+            System.out.println(model + "의 엔진을 구동합니다.");
+        }
+    }
+}
